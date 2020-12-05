30c35f2466f7f17bb19a53e4a6719b79088a89d8
Patrick <patricklang87@gmail.com>
2020-12-05 18:21:36 -0500

    began html page

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-// @ts-check
 import { prepSentence, arToPhraseString} from './textPrep';
 import { sentenceParser } from './parserBeta';
 import { wordOrderEditor } from './wordOrderEditor';
 import { autocorrect } from './wordSpellingEditor';
 
-
-
-
+/*
 let phrase1 = "Ich bin vom weiten gekommen.";
 let phrase2 = "monkey weiten glass gekommen bi.";
+*/
+
 
 const altAr = (ar) => {
     let newAr = [];
@@ -16,9 +15,7 @@ const altAr = (ar) => {
     return newAr;
 }
 
-
 const calculateEdits = (keyAns, userAns) => {
-
     // break the comparison sentences into individual words
     let userAnsPrepped = prepSentence(userAns);
     let keyAnsPrepped = prepSentence(keyAns);
@@ -39,26 +36,55 @@ console.log("STEP 1 (tokenize): key status: ", keyAnsPrepped, "userans status: "
     let numDeletedWords = autocorrectedResult[2];
     console.log("STEP 3 (autocorrect and delete): keystatus: ", groupedKeyAnsPrepped , " userans status: ", autocorrectedUserAns );
 
-
     // turn the internal arrays back into strings
     let userSectionString = arToPhraseString(autocorrectedUserAns);
     let keySectionString = arToPhraseString(groupedKeyAnsPrepped);
 
     console.log("STEP 4 (return inner ars to strings): key status: ", keySectionString, " userans status: ", userSectionString);
 
-
-
     let reorderedUserSub = wordOrderEditor(keySectionString, userSectionString);
     let reorderedPhrase = reorderedUserSub[0];
     let reorderCount = reorderedUserSub[1];
     let numInsertedWords = reorderedUserSub[2];
     
-  
     return [reorderedPhrase, numAutocorrectedWords, numDeletedWords, numInsertedWords, reorderCount];
- 
+}
+
+
+/*let outcome = calculateEdits(phrase1, phrase2);
+console.log("Edited Phrase: ", outcome[0], ", Spelling Autocorrections: ", outcome[1], ", Item Removals: ", outcome[2], ", Item insertions: ", outcome[3], ", Rearrangement Moves: ", outcome[4]);*/
+
+// html interactivity section
+
+
+
+const englishTest = () => {
+    console.log("begin english test");
+    let englishAnsKey = "My sister wants to try to eat healthily.";
+    let userResponse = document.getElementById("english-response").value;
+    let outcome = calculateEdits(englishAnsKey, userResponse);
+    console.log("working");
+    document.getElementById("user-english-response").innerHTML = userResponse;
+    document.getElementById("closest-result").innerHTML = outcome[0];
+    document.getElementById("english-spelling-autocorrections").innerHTML = outcome[1];
+    document.getElementById("english-deletions").innerHTML = outcome[2];
+    document.getElementById("english-insertions").innerHTML = outcome[3];
+    document.getElementById("english-WOEs").innerHTML = outcome[4];
 
 }
 
 
-let outcome = calculateEdits(phrase1, phrase2);
-console.log("Edited Phrase: ", outcome[0], ", Spelling Autocorrections: ", outcome[1], ", Item Removals: ", outcome[2], ", Item insertions: ", outcome[3], ", Rearrangement Moves: ", outcome[4]);
+
+let engSubButton = document.getElementById("english-button");
+engSubButton.addEventListener('click', englishTest);
+
+
+/*
+const showUserInput = () => {
+	let userInput = document.getElementById("english-response").value;
+  console.log(userInput);
+  document.getElementById("user-english-response").innerHTML = userInput;
+}
+
+document.getElementById("english-button").addEventListener('click', showUserInput);
+*/
